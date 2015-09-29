7f10a7552b56e04fe05cc13983b7abff90f67f67
neebz <muneeb83@gmail.com>
2015-09-29 16:58:14 +0500

    image slide component added

---
+++ src/slides/image-slide.jsx
@@ -0,0 +1,37 @@
+import React from 'react/react'
+import Heading from './heading.jsx'
+import { Panel } from 'react-bootstrap';
+
+class ImageSlide extends React.Component  {
+  render() {
+
+    let divStyle = {
+      margin: "auto",
+      width: "90%",
+      height: "80%",
+    }
+
+    let boxesStyle = {
+      margin:"auto",
+      width:"100%",
+      textAlign: "center",
+    }
+  	
+    return  <div>
+              <Heading title="Our inbox" />
+              <div style={divStyle}>
+                <div style={{display:"flex", flexDirection:"column", height:"100%"}}>
+                  
+                  <div style={boxesStyle}>
+                    <Panel> 
+                      <img src={this.props.path} />
+                    </Panel>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+  }
+}
+
+export default ImageSlide;
