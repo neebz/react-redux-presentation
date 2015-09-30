2c59174caa312161a06d940dccc4c0a71ee21d98
neebz <muneeb83@gmail.com>
2015-09-30 14:41:47 +0500

    change port to 80

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname));
 
-var server = app.listen(8000, function () {
+var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
